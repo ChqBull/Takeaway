fb20e68bfb88af2f5e3eded34ab3b5369d8db0a3
ChqBull <1316237778@qq.com>
2019-06-12 20:03:24 +0800

    Eureka

---
+++ TakeawayEureka/src/test/java/com/jk/takeawayeureka/TakeawayeurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jk.takeawayeureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TakeawayeurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
