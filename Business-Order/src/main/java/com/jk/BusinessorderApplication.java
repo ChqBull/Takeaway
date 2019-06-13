4f10e8ecb90c07547b4d9f9f9fff356431931a73
ChqBull <1316237778@qq.com>
2019-06-13 14:51:09 +0800

    6.13 框架

---
+++ Business-Order/src/main/java/com/jk/BusinessorderApplication.java
@@ -0,0 +1,20 @@
+package com.jk;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication/*开启入口类*/
+@EnableDiscoveryClient//表明是客户端,可以往注册中心注册
+@EnableEurekaClient
+@MapperScan("com.jk.mapper")/*扫描持久层*/
+public class BusinessorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessorderApplication.class, args);
+    }
+
+}
---
+++ Total-Webmaster/src/main/java/com/jk/TotalwebmasterApplication.java
@@ -0,0 +1,19 @@
+package com.jk;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication/*开启入口类*/
+@EnableDiscoveryClient//表明是客户端,可以往注册中心注册
+@EnableEurekaClient
+@MapperScan("com.jk.mapper")/*扫描持久层*/
+public class TotalwebmasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TotalwebmasterApplication.class, args);
+    }
+
+}
