964e2221873e0d0ead6e5f58f58698c8f6cf2e42
cmf123666 <825916511@qq.com>
2019-06-20 10:08:00 +0800

    沐繁第三次提交代码

---
+++ User/src/main/java/com/jk/bean/Area.java
@@ -0,0 +1,12 @@
+package com.jk.bean;
+
+import lombok.Data;
+
+import java.io.Serializable;
+@Data
+public class Area implements Serializable {
+     private Integer id;
+     private String areaName;
+     private String pid;
+
+}
